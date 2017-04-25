38548bb0843a17151ef5eb6eb34c8bb26fd649fd
Gregory Hershkowitz <gregoryhershkowitz@gmail.com>
2017-04-25 19:37:58 +0300

    Cleaned up code, added comments

--- mediaMover.py
+++ mediaMover.py
@@ -3,37 +3,48 @@ import shutil
 from Tkinter import Tk
 from tkFileDialog import askdirectory
 
-Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
+Tk().withdraw()
 # show an "Open" dialog box and return the path to the selected file
+
+# array to hold folder names to delete
 foldersToDeleteArray = []
+
 # ask for the directory the user wants to use
 location = askdirectory()
 
-
+# read extension names from text file
 with open("extensionsToMove.txt") as f:
     extensionsToMove = f.readlines()
-# you may also want to remove whitespace characters like `\n` at the end
-# of each line
-extensionsToMove = [x.strip() for x in extensionsToMove]
 
+# removes whitespace from each line
+extensionsToMove = [lines.strip() for lines in extensionsToMove]
+
+# size of extension array
 extensionListSize = len(extensionsToMove)
 
 
 for path, subdirs, files in os.walk(location):
     for filename in sorted(files):
         f = os.path.join(path, filename)
-        # searching for files ending with this extensions
+
+        # loop through each extension from extensionsToMove array
         for num in range(0, extensionListSize):
+
             if filename.endswith(extensionsToMove[num]):
-                SearchResultFileName = (os.path.split(f)[1])
-                # print "this is searchResult " + str(SearchResultFileName)
+
+                # folder two directories up, moving file here
                 twoFoldersUp = os.path.normpath(os.path.join(f, '../../'))
 
+                # folder file is currently in
                 oneFolderUp = os.path.normpath(os.path.join(f, '../'))
+
+                # create array of folders to delete
                 if(oneFolderUp not in foldersToDeleteArray):
                     foldersToDeleteArray.append(oneFolderUp)
 
+                # move file (f) to the directory twoFoldersUp
                 shutil.move(f, twoFoldersUp)
 
+# delete all folders in foldersToDeleteArray
 for item in foldersToDeleteArray:
     shutil.rmtree(item)
